416a4226468332bb46d2b11d58dcda4ebb5ceec3
Om Patil <patil4om@gmail.com>
2025-11-24 11:20:03 +0530

    Used Localstorage to store data

--- src/features/cart/CartSlice.js
+++ src/features/cart/CartSlice.js
@@ -1,9 +1,9 @@
 import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = {
-  value: [...JSON.parse(localStorage.getItem("cartItems")) || []],
-  quantity: [...JSON.parse(localStorage.getItem("quantity")) || []],
-  total: 0
+  value: [...(JSON.parse(localStorage.getItem("cartItems"))) || []],
+  quantity: [...(JSON.parse(localStorage.getItem("quantity"))) || []] ,
+  total: (JSON.parse(localStorage.getItem("totalCartAmount"))) || 0
 }
 
 export const counterSlice = createSlice({
@@ -20,14 +20,14 @@ export const counterSlice = createSlice({
         newQuanity.push(1);
       }
       state.value = updated;
-      localStorage.removeItem("cartItems");
       localStorage.setItem("cartItems", JSON.stringify(state.value))
 
 
       state.quantity = newQuanity;
-      localStorage.removeItem("quantity");
       localStorage.setItem("quantity", JSON.stringify(state.quantity))
+
       state.total += Math.floor(action.payload.price * 88);
+      localStorage.setItem("totalCartAmount", JSON.stringify(state.total))
     },
     removeFromCart: (state, action) => {
       const index = state.value.findIndex(ele => ele.id === action.payload.id);
@@ -35,14 +35,14 @@ export const counterSlice = createSlice({
       if (index !== -1) {
         const valueToRemove = (state.quantity[index] * (action.payload.price * 88));
         state.value = state.value.filter((ele) => ele.id !== action.payload.id);
-        localStorage.removeItem("cartItems");
         localStorage.setItem("cartItems", JSON.stringify(state.value))
 
 
         state.quantity = state.quantity.filter((_, i) => i !== index);
-        localStorage.removeItem("quantity");
         localStorage.setItem("quantity", JSON.stringify(state.quantity))
+
         state.total -= Math.floor(valueToRemove);
+        localStorage.setItem("totalCartAmount", JSON.stringify(state.total))
       }
 
 
@@ -51,20 +51,24 @@ export const counterSlice = createSlice({
       const index = state.value.findIndex(ele => ele.id === action.payload.id);
       if (index !== -1) {
         state.quantity[index] += 1;
-        localStorage.removeItem("quantity");
         localStorage.setItem("quantity", JSON.stringify(state.quantity))
+
         state.total += Math.floor(action.payload.price * 88);
+        localStorage.setItem("totalCartAmount", JSON.stringify(state.total))
       }
 
     },
     clearCartItems: (state, action) => {
       state.value = [];
-      localStorage.removeItem("cartItems");
       localStorage.setItem("cartItems", JSON.stringify(state.value))
+
+
       state.quantity = [];
-      localStorage.removeItem("quantity");
       localStorage.setItem("quantity", JSON.stringify(state.quantity))
+
+
       state.total = 0;
+      localStorage.setItem("totalCartAmount", JSON.stringify(state.total))
     },
 
     decreaseQuantity: (state, action) => {
@@ -73,7 +77,10 @@ export const counterSlice = createSlice({
         state.quantity[index] -= 1;
         localStorage.removeItem("quantity");
         localStorage.setItem("quantity", JSON.stringify(state.quantity))
+
         state.total -= Math.floor(action.payload.price * 88);
+        localStorage.removeItem("totalCartAmount");
+        localStorage.setItem("totalCartAmount", JSON.stringify(state.total))
       }
     },
   },
